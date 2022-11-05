30b7e19efa24f0a480c65f7d6f23ed489610acef
Thomas Moraes <tho.vini7@gmail.com>
2022-11-05 11:56:25 -0300

    Edicao de cursos

---
+++ src/Controller/FormularioEdicao.php
@@ -0,0 +1,31 @@
+<?php 
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class FormularioEdicao implements InterfaceControlador
+{
+    private $entityManager;
+    private $repositorioCursos;
+
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator())->getEntityManager(); 
+        $this->repositorioCursos = $this->entityManager->getRepository(Curso::class);
+    }
+
+    public function processaRequisicao(): void
+    {
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+        if(is_null($id) || $id === false) {
+            header('Location: /listar-cursos');
+            return;
+        }
+
+        $curso = $this->repositorioCursos->find($id);
+        $titulo = "Alterar curso:" . $curso->getDescricao();
+        require __DIR__ . "/../../view/cursos/formulario-novo-curso.php";
+    }
+}
